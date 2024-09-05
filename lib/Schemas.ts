27713f414569a734e6a3273002973e3e807498d1
Tufailahmed <bargirtufailahmed@gmail.com>
2024-09-05 11:36:24 +0530

    resolved the vercel error

--- lib/Schemas.ts
+++ lib/Schemas.ts
@@ -1,28 +1,6 @@
 import { z } from "zod";
 
-export const convertSchema = z.object({
-  legacyLanguage: z.string().min(1, { message: "legacy Language is required" }),
-
-  Code: z.string().min(1, { message: "legacy  Code is required" }),
-
-  modernLanguage: z.string().min(1, { message: "Modern Language is required" }),
-});
-
-export const userSchema = z.object({
-  name: z
-    .string()
-    .min(1, { message: "Name is required" })
-    .regex(/^[a-zA-Z]+$/, { message: "Only alphabets are allowed" }),
-
-  email: z.string().email({ message: "Email is required" }),
-
-  password: z
-    .string()
-    .min(1, { message: "Password is required" })
-    .regex(/^[a-zA-Z0-9]+$/, {
-      message: "Only alphanumeric characters are allowed",
-    }),
-});
+ 
 
 export const convertSchemaInput = z.object({
   legacyLanguage: z
@@ -35,3 +13,5 @@ export const convertSchemaInput = z.object({
       .string()
       .min(1, { message: "Modern Language is required" }),
   });
+
+  
